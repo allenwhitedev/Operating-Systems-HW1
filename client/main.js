c9f7202b6f9d47585caa037346db454c387422b3
allenwhitedev <allenwhitedev@gmail.com>
2016-11-03 23:36:09 -0400

    Add functionality for resolving endtime ties

--- client/main.js
+++ client/main.js
@@ -35,7 +35,7 @@ for ( let y = 0; y < P.length; y++ )
 	if ( minArrayWidth > P[y].length )
 	{
 		minArrayWidth = P[y].length
-		//console.log("P" + y + P[y].length)
+		console.log("P" + y + " " + P[y].length)
 	}
 
 
@@ -71,14 +71,25 @@ let calculateFCFS = function()
 
 			if (iOBurst == undefined) iOBurst = 0
 
-			// store end time
-			let currProgram = readyQueue[readyQueue.length - 1][y]
-			tmpEndTimes[currProgram] = cpuTime + cpuBurst + iOBurst 
+			// store end time]
+			tmpEndTimes[nextProgram] = cpuTime + cpuBurst + iOBurst 
 
 			// store wait time in an array parallel to end times array
-			tmpWaitTimes[currProgram] = cpuTime - endTimes[endTimes.length - 1][nextProgram]
+			let tmpWaitTime = cpuTime - endTimes[endTimes.length - 1][nextProgram]
+			if (tmpWaitTime >= 0)
+				tmpWaitTimes[nextProgram] = cpuTime - endTimes[endTimes.length - 1][nextProgram]
 			
 			cpuTime += cpuBurst // update cpu time
+
+			// turnaround time for finished programs
+			if (x >= P[nextProgram].length - 1)
+			{
+				//console.log("endTime", tmpEndTimes[nextProgram] )
+				//console.log('cpuTime', cpuTime)
+				let finishTime = cpuTime + iOBurst
+				console.log("P" + nextProgram + " has finished with " + cpuBurst + " @ " + finishTime)
+				break
+			}
 			//console.log(cpuBurst, iOBurst)
 		}
 
@@ -92,10 +103,25 @@ let calculateFCFS = function()
 		for (let i = 0; i < tmpEndTimes.length; i++)
 		{
 			let nextReadyQueueProgram = tmpEndTimes.indexOf(tmpEndTimesSorted[i])
+			
+			// resolve end time ties
+			if (tmpReadyQueue.indexOf(nextReadyQueueProgram) != -1)
+			{
+				console.log("tie!", tmpEndTimesSorted[i])
+				
+				let endTimeTie = tmpEndTimesSorted[i]
+				
+				// run through other end times and find next occurence of end time  
+				// that is NOT in ready queue then setup to be added to ready queue
+				for (let j = nextReadyQueueProgram + 1; j < tmpEndTimes.length; j++)
+					if (tmpEndTimes[j] == endTimeTie && tmpReadyQueue.indexOf(j) == -1)
+						nextReadyQueueProgram = j			
+			}
+
 			if (nextReadyQueueProgram != -1)
 				tmpReadyQueue.push( nextReadyQueueProgram )			
 		}
-
+		console.log("tmpReadyQueue", tmpReadyQueue)
 		// update multidimension arrays @ end of each iteration
 		readyQueue[readyQueue.length] = tmpReadyQueue
 		waitTimes[waitTimes.length] = tmpWaitTimes
@@ -110,6 +136,15 @@ let calculateFCFS = function()
 		console.log("waitTimes", waitTimes)
 		console.log("readyQueue", readyQueue)
 
+
+		let p0WaitTime = 0
+		for (let i = 0; i < waitTimes.length; i++)
+			if (waitTimes[i][0] != undefined)
+				p0WaitTime += waitTimes[i][0]
+		//	console.log(waitTimes[i][0])
+		//	p0WaitTime += waitTimes[i][0]
+
+		console.log("p0WaitTime", p0WaitTime)
 }
 
 
